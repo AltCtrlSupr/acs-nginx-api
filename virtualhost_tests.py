082e17fc8405ed0b4348b912d09926b2b7c94702
Genar Trias Ortiz <genar@acs.li>
2016-03-22 20:48:35 +0100

    first test running OK

--- virtualhost_tests.py
+++ virtualhost_tests.py
@@ -1,23 +1,27 @@
-import os
-import main
-import unittest
+from main import app
+import pytest
 import tempfile
 
-class FlaskrTestCase(unittest.TestCase):
+@pytest.fixture
+def client(request):
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+    client = app.test_client()
+    # with app.app_context():
+        # init_db()
 
-    def setUp(self):
-        self.db_fd, main.app.config['DATABASE'] = tempfile.mkstemp()
-        main.app.config['TESTING'] = True
-        self.app = main.app.test_client()
-        # main.init_db()
+    # def teardown():
+        # os.close(db_fd)
+        # os.unlink(app.config['DATABASE'])
+    # request.addfinalizer(teardown)
 
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(main.app.config['DATABASE'])
+    return client
 
-    def test_virtual_host_index(self):
-        rv = self.app.get('/')
-        assert 'whatever' in rv.data
+def test_virtual_host_index(client):
+    rv = client.get('/virtualhost')
+    assert rv._status_code == 200
 
-if __name__ == '__main__':
-    unittest.main()
+    # print vars(rv)
+    # assert 1 != 1
+    # assert 'whatever' in rv.data
+
