c8f1df0f7cd1e853f4a1576c739a42148b7ab617
ZUNbado <git@acs.li>
2016-03-22 20:45:47 +0100

    fixing mongo connection

--- dev.py
+++ dev.py
@@ -1,5 +1,7 @@
 from main import app
+from mongo import mongo
 
 if __name__ == '__main__':
     app.config.from_object('config.Development')
+    mongo.init_app(app)
     app.run(debug=True)
--- resources/virtualhost.py
+++ resources/virtualhost.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, Blueprint, current_app, jsonify, url_for, redirect, request
-from flask_pymongo import PyMongo
-from flask_restful import Api, Resource
+from flask.ext.restful import Api, Resource
+from mongo import mongo
 
 app = Flask(__name__)
 api_bp = Blueprint(__name__, __name__)
 api = Api(api_bp)
 
 class VirtualHost(Resource):
-    def __init__(self):
-        self.mongo = PyMongo(current_app)
-
     def get(self, slug = None):
         data = []
 
-        cursor = self.mongo.db.virtualhost.find({}, {"_id": 0, "update_time": 0}).limit(10)
+        cursor = mongo.db.virtualhost.find({}, {"_id": 0, "update_time": 0}).limit(10)
 
         for virtualhost in cursor:
             print(virtualhost)
@@ -32,17 +29,17 @@ class VirtualHost(Resource):
             #if mongo.db.virtualhost.find_one({"slug": slug}):
                 #return {"response": "student already exists."}
             #else:
-            self.mongo.db.virtualhost.insert(data)
+            mongo.db.virtualhost.insert(data)
 
         return redirect(url_for("virtualhosts"))
 
     def put(self, slug):
         data = request.get_json()
-        self.mongo.db.virtualhost.update({'slug': slug}, {'$set': data})
+        mongo.db.virtualhost.update({'slug': slug}, {'$set': data})
         return redirect(url_for("virtualhosts"))
 
     def delete(self, slug):
-        self.mongo.db.virtualhost.remove({'slug': slug})
+        mongo.db.virtualhost.remove({'slug': slug})
         return redirect(url_for("virtualhosts"))
 
 api.add_resource(VirtualHost, "/virtualhost", endpoint="virtualhosts")
