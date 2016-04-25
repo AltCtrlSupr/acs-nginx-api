ae9c7f476399c9aa452cec739f6888932b41a3af
Genar Trias Ortiz <genar@acs.li>
2016-04-25 10:45:53 +0200

    putting schemas in its own files for easily importing when they are needed

--- resources/virtualhost.py
+++ resources/virtualhost.py
@@ -2,56 +2,13 @@
 from flask import Flask, Blueprint
 from flask.ext.restful import Api
 from resources.base import BaseResource
+from schemas import virtualhost
+
+virtual_host_schema = virtualhost.virtual_host_schema
 
 app = Flask(__name__)
 api_bp = Blueprint(__name__, __name__)
 api = Api(api_bp)
-
-virtual_host_schema = {
-    'name': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 255,
-        'required': True,
-        # 'unique': True,
-    },
-    'ssl_cert': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 255,
-        'required': False,
-    },
-    'ssl_key': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 255,
-        'required': False,
-    },
-    'ssl_chain': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 255,
-        'required': False,
-    },
-    'ports_plain': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 255,
-        'required': False,
-    },
-    'ports_ssl': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 255,
-        'required': False,
-    },
-    'virtual_host_alias': {
-	'type': 'string',
-        'minlength': 1,
-        'maxlength': 255,
-        'required': False,
-    }
-}
 
 class VirtualHost(BaseResource):
     def __init__(self):
---
+++ schemas/virtualhost.py
@@ -0,0 +1,48 @@
+virtual_host_schema = {
+    'name': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 255,
+        'required': True,
+        # 'unique': True,
+    },
+    'ssl_cert': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 255,
+        'required': False,
+    },
+    'ssl_key': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 255,
+        'required': False,
+    },
+    'ssl_chain': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 255,
+        'required': False,
+    },
+    'ports_plain': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 255,
+        'required': False,
+    },
+    'ports_ssl': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 255,
+        'required': False,
+    },
+    'virtual_host_alias': {
+	'type': 'objectid',
+        'data_relation': {
+            'resource': 'virtual_host_alias',
+            'field': '_id',
+            'embeddable': True
+        }
+    }
+}
+
